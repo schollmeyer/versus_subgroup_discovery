8d796157b32abe09296d00a5f6602c49df99083d
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2024-02-05 12:19:07 +0100

    Update examples_versus_relations_phylogenetics.R

--- examples_versus_relations_phylogenetics.R
+++ examples_versus_relations_phylogenetics.R
@@ -4,3 +4,20 @@ tree_1 <- anoletree
 pdf("tree_1.pdf",width=20,height=20)
 plot(tree_1)
 dev.off()
+
+tree_2 <- force.ultrametric(tree_1)
+
+pdf("tree_2.pdf",width=20,height=20)
+plot(tree_2)
+dev.off()
+
+
+
+D1 <- cophenetic.phylo(tree_1)
+D2 <- cophenetic.phylo(tree_2)
+
+data(anole.data)
+y <- anole.data[,1]
+y <- y > median(y)
+y <- oofos::compute_objective(data.frame(y=y),"y","TRUE")
+table(y)
